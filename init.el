6837a13417310ba390f5169e67041b7848a7f540
Brett Hoerner <bretthoerner@gmail.com>
2009-06-20 09:37:07 -0500

    Friendly binding for find-file-in-project.

--- init.el
+++ init.el
@@ -354,7 +354,7 @@ makes)."
 
 ;; file finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
-(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-x t") 'find-file-in-project)
 (global-set-key (kbd "C-x f") 'recentf-ido-find-file)
 (global-set-key (kbd "C-x C-p") 'find-file-at-point)
 (global-set-key (kbd "C-c y") 'bury-buffer)
