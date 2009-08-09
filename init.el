c3b22f26ec4f321a751c3794d36c560a4e1a1204
Brett Hoerner <bretthoerner@gmail.com>
2009-08-09 11:17:26 -0700

    Fix dired-single bindings.

--- init.el
+++ init.el
@@ -88,15 +88,17 @@
 (defun my-dired-init ()
   "Bunch of stuff to run for dired, either immediately or when it's
    loaded."
-  (define-key dired-mode-map [return] 'joc-dired-single-buffer)
-  (define-key dired-mode-map [mouse-1] 'joc-dired-single-buffer-mouse)
+  ;; <add other stuff here>
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
   (define-key dired-mode-map "^"
-        (function
-         (lambda nil (interactive) (joc-dired-single-buffer "..")))))
-;; if dired is already loaded, then the keymap will be bound
+    (function
+     (lambda nil (interactive) (dired-single-buffer "..")))))
+
+;; if dired's already loaded, then the keymap will be bound
 (if (boundp 'dired-mode-map)
-        ;; we're good to go; just add our bindings
-        (my-dired-init)
+    ;; we're good to go; just add our bindings
+    (my-dired-init)
   ;; it's not loaded yet, so add our bindings to the load-hook
   (add-hook 'dired-load-hook 'my-dired-init))
 
