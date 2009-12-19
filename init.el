cee293d03e9d45c3bce43a2bd7d00a979b6441c0
Brett Hoerner <brett@bretthoerner.com>
2009-12-19 11:34:01 -0800

    Update Slime, add bnf-mode.

--- init.el
+++ init.el
@@ -42,6 +42,21 @@
 ;; ansi-color
 (require 'ansi-color)
 
+;; bnf-mode
+(define-generic-mode 'bnf-mode
+ () ;; comment char: inapplicable because # must be at start of line
+ nil ;; keywords
+ '(
+   ("^#.*" . 'font-lock-comment-face) ;; comments at start of line
+   ("^<[^ \t\n]*?>" . 'font-lock-function-name-face) ;; LHS nonterminals
+   ("<[^ \t\n]*?>" . 'font-lock-builtin-face) ;; other nonterminals
+   ("::=" . 'font-lock-const-face) ;; "goes-to" symbol
+   ("\|" . 'font-lock-warning-face) ;; "OR" symbol
+   )
+ '("\\.bnf\\'") ;; filename suffixes
+ nil ;; extra function hooks
+ "Major mode for BNF highlighting.")
+
 ;; browse-kill-ring
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)
@@ -275,6 +290,7 @@ makes)."
 (require 'slime)
 (global-set-key "\C-cs" 'slime-selector)
 (add-to-list 'slime-lisp-implementations '(allegro ("alisp")))
+(add-to-list 'slime-lisp-implementations '(sbcl ("sbcl")))
 (slime-setup)
 
 ;; smooth-scrolling for keyboard
