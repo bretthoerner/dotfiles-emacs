0d19136ac86d3b4bf8f288854c9c3e67dcf273d9
Brett Hoerner <brett@bretthoerner.com>
2010-11-21 17:32:07 -0600

    Re-enable malabar-mode, but check that the dir exists first.

--- init.el
+++ init.el
@@ -291,16 +291,18 @@ makes)."
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; malabar-mode
-;(add-to-list 'load-path (concat dotfiles-dir "malabar/lisp"))
-;(require 'cedet)
-;(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-;                                  global-semanticdb-minor-mode
-;                                  global-semantic-idle-summary-mode
-;                                  global-semantic-mru-bookmark-mode))
-;(require 'malabar-mode)
-;(add-hook 'malabar-mode-hook (lambda ()  (semantic-mode 1)))
-;(setq malabar-groovy-lib-dir (concat dotfiles-dir "malabar/lib"))
-;(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+(let ((malabar-dir (concat dotfiles-dir "malabar-mode/")))
+  (when (file-exists-p malabar-dir)
+    (add-to-list 'load-path (concat malabar-dir "lisp"))
+    (require 'cedet)
+    (setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                      global-semanticdb-minor-mode
+                                      global-semantic-idle-summary-mode
+                                      global-semantic-mru-bookmark-mode))
+    (require 'malabar-mode)
+    (add-hook 'malabar-mode-hook (lambda ()  (semantic-mode 1)))
+    (setq malabar-groovy-lib-dir (concat malabar-dir "lib"))
+    (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))))
 
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
