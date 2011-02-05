b6f9403924cf479bf7652b4e7b596aa20546e071
Brett Hoerner <brett@bretthoerner.com>
2011-02-05 13:50:46 -0600

    Updates to rcirc config and fonts.

--- init.el
+++ init.el
@@ -401,8 +401,6 @@ makes)."
    (require 'rcirc-notify)
    ;; (require 'rcirc-notify-mode)
 
-   (rcirc-track-minor-mode 1)
-
    ;; clear screen
    ;; (let ((inhibit-read-only t))
    ;;   (delete-region
@@ -410,12 +408,39 @@ makes)."
 
    (add-hook 'rcirc-mode-hook
              '(lambda ()
-                (set (make-local-variable 'blink-matching-paren) nil)))
+                (progn
+                  (set (make-local-variable 'blink-matching-paren) nil)
+                  (set (make-local-variable 'scroll-conservatively) 8192)
+                  (rcirc-track-minor-mode 1)
+                  (flyspell-mode 1))))
+
+   (defun-rcirc-command reconnect (arg)
+     "Reconnect the server process."
+     (interactive "i")
+     (unless process
+       (error "There's no process for this target"))
+     (let* ((server (car (process-contact process)))
+            (port (process-contact process :service))
+            (nick (rcirc-nick process))
+            channels query-buffers)
+       (dolist (buf (buffer-list))
+         (with-current-buffer buf
+           (when (eq process (rcirc-buffer-process))
+             (remove-hook 'change-major-mode-hook
+                          'rcirc-change-major-mode-hook)
+             (if (rcirc-channel-p rcirc-target)
+                 (setq channels (cons rcirc-target channels))
+               (setq query-buffers (cons buf query-buffers))))))
+       (delete-process process)
+       (rcirc-connect server port nick
+                      rcirc-default-user-name
+                      rcirc-default-user-full-name
+                      channels)))
 
    (setq rcirc-default-nick "brett_h"
          rcirc-default-user-name "brett"
          rcirc-default-full-name "Brett"
-         rcirc-keywords '("brett")
+         rcirc-keywords '("brett" "bretthoerner" "hoerner")
          rcirc-authinfo `(("freenode" nickserv "brett_h" ,bjh-freenode-password)
                           ("localhost" bitlbee "brett_h" ,bjh-bitlbee-password))
          rcirc-server-alist '(("irc.freenode.net" :channels ("#disqus"))
