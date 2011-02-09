273fd50170e0e5a9bbe13a31f7f7bc0afcf58324
Brett Hoerner <brett@bretthoerner.com>
2011-02-09 16:30:52 -0600

    Better C-a, smart-beginning-of-line.

--- init.el
+++ init.el
@@ -754,6 +754,20 @@ makes)."
 (add-to-list 'same-window-buffer-names "*Apropos*")
 (add-to-list 'same-window-buffer-names "*Summary*")
 
+;; better C-a
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key "\C-a" 'smart-beginning-of-line)
+
 ;; lolworthy function from esk
 (defun bjh-disapproval ()
   (interactive)
