8ed3362c2a89897a5f67004b988f5605aadf0d95
Brett Hoerner <brett@bretthoerner.com>
2011-02-18 08:15:46 -0600

    Add new python.el as submodule, update swank-clojure.

--- init.el
+++ init.el
@@ -149,56 +149,6 @@
 (setq ffip-patterns
       '("*.clj" "*.css" "*.el" "*.html" "*.js" "*.py" "*.rb" "*.txt"))
 
-;; flymake
-(setq-default flymake-gui-warnings-enabled nil)
-(require 'flymake)
-(load-library "flymake-cursor")
-
-(defun flymake-create-temp-intemp (file-name prefix)
-  "Return file name in temporary directory for checking FILE-NAME.
-This is a replacement for `flymake-create-temp-inplace'. The
-difference is that it gives a file name in
-`temporary-file-directory' instead of the same directory as
-FILE-NAME.
-
-For the use of PREFIX see that function.
-
-Note that not making the temporary file in another directory
-\(like here) will not if the file you are checking depends on
-relative paths to other files \(for the type of checks flymake
-makes)."
-  (unless (stringp file-name)
-    (error "Invalid file-name"))
-  (or prefix
-      (setq prefix "flymake"))
-  (let* ((name (concat
-                (file-name-nondirectory
-                 (file-name-sans-extension file-name))
-                "_" prefix))
-         (ext  (concat "." (file-name-extension file-name)))
-         (temp-name (make-temp-file name nil ext))
-         )
-    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
-    temp-name))
-
-(defun flymake-pyflakes-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-intemp))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "pyflakes" (list local-file))))
-
-(push '("\\.py\\'" flymake-pyflakes-init)
-      flymake-allowed-file-name-masks)
-(push '("could not compile '\\([^']+\\)':\\([0-9]+\\):\\(\n.*\\)" 1 2 nil nil)
-      flymake-err-line-patterns)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            ;; Activate flymake unless buffer is a tmp buffer for the interpreter
-            (unless (eq buffer-file-name nil) (flymake-mode t))))
-
 ;; flyspell-mode
 (setq ispell-program-name "aspell"
       ispell-extra-args '("--sug-mode=ultra"))
@@ -431,6 +381,59 @@ makes)."
 (require 'puppet-mode)
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 (setq puppet-indent-level 4)
+
+;; python
+(add-to-list 'load-path (concat dotfiles-dir "python"))
+(require 'python)
+
+(setq-default flymake-gui-warnings-enabled nil)
+(require 'flymake)
+(load-library "flymake-cursor")
+
+(defun flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking FILE-NAME.
+This is a replacement for `flymake-create-temp-inplace'. The
+difference is that it gives a file name in
+`temporary-file-directory' instead of the same directory as
+FILE-NAME.
+
+For the use of PREFIX see that function.
+
+Note that not making the temporary file in another directory
+\(like here) will not if the file you are checking depends on
+relative paths to other files \(for the type of checks flymake
+makes)."
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
+
+(defun flymake-pyflakes-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-intemp))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "pyflakes" (list local-file))))
+
+(push '("\\.py\\'" flymake-pyflakes-init)
+      flymake-allowed-file-name-masks)
+(push '("could not compile '\\([^']+\\)':\\([0-9]+\\):\\(\n.*\\)" 1 2 nil nil)
+      flymake-err-line-patterns)
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            ;; Activate flymake unless buffer is a tmp buffer for the interpreter
+            (unless (eq buffer-file-name nil) (flymake-mode t))))
 
 ;; quack
 (require 'quack)
