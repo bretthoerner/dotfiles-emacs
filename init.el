8e003e9b1c7f1392d54021eb94909ffcf543778a
Brett Hoerner <brett@bretthoerner.com>
2011-05-11 10:35:25 -0500

    Bind - in dired to use single-dired.

--- init.el
+++ init.el
@@ -104,15 +104,15 @@
 (require 'dired-x)
 (setq dired-omit-files-p t)
 
-;; - for cd .. (^)
-(define-key dired-mode-map "-" 'dired-up-directory)
-
 (defun my-dired-init ()
   "Bunch of stuff to run for dired, either immediately or when it's
    loaded."
   (define-key dired-mode-map [return] 'dired-single-buffer)
   (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
   (define-key dired-mode-map "^"
+    (function
+     (lambda nil (interactive) (dired-single-buffer ".."))))
+  (define-key dired-mode-map "-"
     (function
      (lambda nil (interactive) (dired-single-buffer "..")))))
 
