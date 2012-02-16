515ce81cc059a64d25a08822d4696bd53cb1b78f
Brett Hoerner <brett@bretthoerner.com>
2012-02-16 10:24:16 -0600

    Load ENSIME.

--- init.el
+++ init.el
@@ -612,6 +612,10 @@ makes)."
 (add-to-list 'load-path (concat dotfiles-dir "scala"))
 (require 'scala-mode-auto)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'load-path "/usr/share/ensime/elisp")
+(add-to-list 'exec-path "/usr/share/ensime")
+(when (require 'ensime nil t)
+  (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
 
 ;; scpaste
 (require 'scpaste)
