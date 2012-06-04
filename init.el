dc5d1c0316a95264bec946ea525b58fcc67a3745
Brett Hoerner <brett@bretthoerner.com>
2012-06-04 09:40:42 -0500

    Update submodules.

--- init.el
+++ init.el
@@ -317,55 +317,6 @@ makes)."
       js2-consistent-level-indent-inner-bracket-p t
       js2-pretty-multiline-decl-indentation-p t)
 
-;; Custom indentation function since JS2 indenting is terrible.
-;; Uses js-mode's (espresso-mode) indentation semantics.
-;;
-;; Based on: http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
-(defun bjh-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (js--proper-indentation parse-status))
-           node)
-
-      (save-excursion
-
-        ;; I like to indent case and labels to half of the tab width
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-            (setq indentation (+ indentation (/ js-indent-level 2))))
-
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;; bar = "foo";
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation (+ 4 indentation))))
-
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
-
-(defun bjh-js2-mode-hook ()
-  (if (not (boundp 'js--proper-indentation))
-      (progn (js-mode)
-             (remove-hook 'js2-mode-hook 'bjh-js2-mode-hook)
-             (js2-mode)
-             (add-hook 'js2-mode-hook 'bjh-js2-mode-hook)))
-  (set (make-local-variable 'indent-line-function) 'bjh-js2-indent-function)
-  (define-key js2-mode-map [(return)] 'newline-and-indent)
-  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
-  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward))
-
-;; Add the hook so this is all loaded when JS2-mode is loaded
-(add-hook 'js2-mode-hook 'bjh-js2-mode-hook)
-
 ;; js-comint
 (require 'js-comint)
 (setq inferior-js-program-command "/usr/bin/rhino")
