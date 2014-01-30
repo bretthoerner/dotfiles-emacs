9c84f47977561810b8e1afe5435a15d242426b7c
Brett Hoerner <brett@bretthoerner.com>
2014-01-30 14:52:22 -0600

    Cleanup.

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;; debug config
 ;; -------------
 
-(setq debug-on-error nil)
+(setq debug-on-error t)
 
 
 ;; -------------
@@ -26,28 +26,36 @@
 (package-initialize)
 
 (defvar bjh-packages
-  '(ag
+  '(ac-nrepl
+    ag
+    auto-complete
     autopair
     browse-kill-ring
     buffer-move
     c-eldoc
+    cider
     clojure-mode
+    clojure-test-mode
     coffee-mode
     color-theme
     dash
     deft
     diminish
     dired-single
+    docker-mode
     enh-ruby-mode
     evil
     expand-region
     find-file-in-project
     flycheck
     flymake
+    flymake-cursor
     flymake-go
     full-ack
+    gh
     gist
     git-commit-mode
+    gitignore-mode
     git-rebase-mode
     gitignore-mode
     gnuplot-mode
@@ -67,6 +75,7 @@
     magit
     markdown-mode
     multi-term
+    nrepl
     paredit
     php-mode
     puppet-mode
@@ -118,37 +127,6 @@
 (require 'flymake)
 (load-library "flymake-cursor")
 
-(defun flymake-create-temp-intemp (file-name prefix)
-  "Return file name in temporary directory for checking FILE-NAME.
-This is a replacement for `flymake-create-temp-inplace'. The
-difference is that it gives a file name in
-`temporary-file-directory' instead of the same directory as
-FILE-NAME.
-
-For the use of PREFIX see that function.
-
-Note that not making the temporary file in another directory
-\(like here) will not if the file you are checking depends on
-relative paths to other files \(for the type of checks flymake
-makes)."
-  (unless (stringp file-name)
-    (error "Invalid file-name"))
-  (or prefix
-      (setq prefix "flymake"))
-  (let* ((name (concat
-                (file-name-nondirectory
-                 (file-name-sans-extension file-name))
-                "_" prefix))
-         (ext  (concat "." (file-name-extension file-name)))
-         (temp-name (make-temp-file name nil ext))
-         )
-    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
-    temp-name))
-
-;; ace-jump-mode
-;(require 'ace-jump-mode)
-;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
 ;; autopair
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
@@ -182,6 +160,12 @@ makes)."
 (setq-default c-eldoc-includes "-I./ -I../ ")
 (load "c-eldoc")
 (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+
+;; cider
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-repl-history-file (expand-file-name "~/.emacs.d/cider-history"))
 
 ;; clojure-mode
 (when (require 'clojure-mode nil t)
@@ -327,15 +311,15 @@ makes)."
    (t (format "%8d" (buffer-size)))))
 
 ;; Modify the default ibuffer-formats
-  (setq ibuffer-formats
-	'((mark modified read-only " "
-		(name 18 18 :left :elide)
-		" "
-		(size-h 9 -1 :right)
-		" "
-		(mode 16 16 :left :elide)
-		" "
-		filename-and-process)))
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
 
 ;; ido
 (setq ido-auto-merge-work-directories-length -1
@@ -530,24 +514,6 @@ makes)."
 ;; python
 (require 'python)
 (setq python-indent-offset 2)
-
-(defun flymake-pyflakes-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-intemp))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "pyflakes" (list local-file))))
-
-(push '("\\.py\\'" flymake-pyflakes-init)
-      flymake-allowed-file-name-masks)
-(push '("could not compile '\\([^']+\\)':\\([0-9]+\\):\\(\n.*\\)" 1 2 nil nil)
-      flymake-err-line-patterns)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            ;; Activate flymake unless buffer is a tmp buffer for the interpreter
-            (unless (eq buffer-file-name nil) (flymake-mode t))))
 
 ;; rainbow-mode
 (require 'rainbow-mode)
@@ -1427,10 +1393,6 @@ buffer-local variable `show-trailing-whitespace'."
     (defun bjh-text-scale-decrease (dec)
       (interactive "p")
       (bjh-set-frame-font-size (- bjh-current-font-size dec)))
-
-    ;; shortcuts to resize fonts
-    (define-key global-map (kbd "C-+") 'bjh-text-scale-increase)
-    (define-key global-map (kbd "C--") 'bjh-text-scale-decrease)
 
     ;; dim parens in lisp-modes
     (defface bjh-paren-face
